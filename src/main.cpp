fec0ae3f92cbc1b0a9b058492d946b43f0cb0e16
TennnyZhuang <zty0826@gmail.com>
2016-12-08 13:33:12 +0800

    read file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <streambuf>
 
-int main(int argc, char* argv[]) {
-    std::cout << "Hello World!" << std::endl;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        cout << "no input file" << endl;
+        return 1;
+    }
+
+    string filename(argv[1]);
+
+    std::ifstream fin(filename);
+
+    if (!fin) {
+        cout << "invalid filename" << endl;
+        return 1;
+    }
+
+    std::string content((std::istreambuf_iterator<char>(fin)),
+                        std::istreambuf_iterator<char>());
+
+    cout << content << endl;
+
+    fin.close();
 }
