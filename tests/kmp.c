cf40cb04e7b1017c861786e0abdfba6de195d22e
TennnyZhuang <zty0826@gmail.com>
2016-12-19 01:15:25 +0800

    kmp

---
+++ tests/kmp.c
@@ -0,0 +1,125 @@
+int strlen(char* str, int maxlen) {
+    int i;
+    int flag;
+    int len;
+
+    flag = 1;
+    len = 0;
+    for (i = 0; i < maxlen; i = i + 1) {
+        if (str[i] == '\0') {
+            flag = 0;
+        }
+        if (flag == 1) {
+            len = len + 1;
+        }
+    }
+    return len;
+}
+
+int and(int b1, int b2) {
+    int andtmp135;
+    andtmp135 = (b1 + b2 == 2);
+    return andtmp135;
+}
+
+int or(int b1, int b2) {
+    int ortmp145;
+    ortmp145 = (b1 + b2 != 0);
+    return ortmp145;
+}
+
+int buildnext(char* bup, int* bunext) {
+    int buplen;
+    buplen = strlen(bup, 100);
+
+    bunext[0] = (0 - 1);
+
+    int buk;
+    int buj;
+    buk = (0 - 1);
+    buj = 0;
+
+    int tmp;
+    tmp = 0;
+
+    for (buj = 0 ;buj < buplen - 1; tmp = tmp + 1) {
+        int cmp12345;
+        cmp12345 = or(buk == 0 - 1, bup[buj] == bup[buk]);
+
+        if (cmp12345 == 1) {
+            buk = buk + 1;
+            buj = buj + 1;
+            bunext[buj] = buk;
+        } else {
+            buk = bunext[buk];
+        }
+
+        if (buk == (0 - 1)) {
+            buk = buk + 1;
+            buj = buj + 1;
+            bunext[buj] = buk;
+        }
+    }
+
+    return 0;
+}
+
+int kmpsearch(char* s, char* p, int* next) {
+    int kmpi;
+    int kmpj;
+    int kmpslen;
+    int kmpplen;
+
+    kmpi = 0;
+    kmpj = 0;
+    kmpslen = strlen(s, 100);
+    kmpplen = strlen(p, 100);
+
+    int tmp2;
+    tmp2 = 0;
+
+    int cmp12;
+    cmp12 = and(kmpi < kmpslen, kmpj < kmpplen);
+
+    for (kmpi = 0; cmp12 != 0; tmp2 = tmp2 + 1) {
+        int cmp4242;
+        cmp4242 = or(kmpj == 0 - 1, s[kmpi] == p[kmpj]);
+
+        if (cmp4242) {
+            kmpi = kmpi + 1;
+            kmpj = kmpj + 1;
+        } else {
+            kmpj = next[kmpj];
+        }
+
+        cmp12 = and(kmpi < kmpslen, kmpj < kmpplen);
+    }
+
+    int kmpretval;
+
+    if (kmpj == kmpplen) {
+        kmpretval = kmpi - kmpj;
+    } else {
+        kmpretval = (0 - 1);
+    }
+
+    return kmpretval;
+}
+
+int main() {
+    char mains[100];
+    char mainp[100];
+    int mainnext[100];
+
+    gets(mains);
+
+    gets(mainp);
+
+    buildnext(mainp, mainnext);
+    int result;
+    result = kmpsearch(mains, mainp, mainnext);
+
+    printf("%d", result);
+
+    return 0;
+}
